71e006388ef60d88ce9f3ae879801416851410a3
SarthakKeshari <55992140+SarthakKeshari@users.noreply.github.com>
2020-04-07 20:14:25 +0530

    Breakdown Of The Code (Let's Code)
    Swap without using a temporary variable

--- swap.c
+++ swap.c
@@ -3,18 +3,21 @@
 int main()
 {
     int a,b;
-
+    //For entering two numbers
     printf("Enter first number:");
     scanf("%d",&a);
     printf("Enter second number:");
     scanf("%d",&b);
 
-    printf("Before swaping :\n   First number = %d \n   Second number = %d\n",a,b);
+    //For showing the output before swapping the numbers
+    printf("Before swapping :\n   First number = %d \n   Second number = %d\n",a,b);
 
+    //Logic for swapping the numbers
     a=a+b;
     b=a-b;
     a=a-b;
 
-    printf("After swaping :\n   First number = %d \n   Second number = %d",a,b);
+    //For showing the output after swapping the numbers
+    printf("After swapping :\n   First number = %d \n   Second number = %d",a,b);
     return 0;
 }
